6dce615bf8f628251f0b802a26ac50d46d428c14
Georgi957 <122177357+Georgi957@users.noreply.github.com>
2023-04-19 15:18:41 +0300

    Update rockPaperScissors.js

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -1,104 +1,77 @@
-function rockPaperScissors(playerTurn) {
+function rockPaperScissors() {
   const readline = require("readline").createInterface({
     input: process.stdin,
     output: process.stdout,
   });
-  let computerRandomNumber = Math.floor(Math.random() * 3) + 1; 
-  let computerMove = "";
-  switch (computerRandomNumber) {
-    case 1:
-      computerMove = "Rock";
-      break;
-    case 2:
-      computerMove = "Paper";
-      break;
-    case 3:
-      computerMove = "Scissors";
-      break;
-  }
-  const rock = "Rock";
-  const paper = "Paper";
-  const scissors = "Scissors";
 
-  let playerMove = function () {
+  function getComputerMove() {
+    let computerRandomNumber = Math.floor(Math.random() * 3) + 1;
+    let computerMove = "";
+    switch (computerRandomNumber) {
+      case 1:
+        computerMove = "Rock";
+        break;
+      case 2:
+        computerMove = "Paper";
+        break;
+      case 3:
+        computerMove = "Scissors";
+        break;
+    }
+    return computerMove;
+  }
+
+  function playRound() {
+    let computerMove = getComputerMove();
+
+    const rock = "Rock";
+    const paper = "Paper";
+    const scissors = "Scissors";
+
     readline.question(
       "Make your move! (Choose R - rock, P - paper, S - scissors): ",
-      string => {
-        playerTurn = string.toLowerCase();
+      (string) => {
+        let playerTurn = string.toLowerCase();
         if (playerTurn === "r" || playerTurn === "rock") {
           playerTurn = rock;
-          result();
+          getResult(playerTurn, computerMove);
         } else if (playerTurn === "p" || playerTurn === "paper") {
           playerTurn = paper;
-          result();
+          getResult(playerTurn, computerMove);
         } else if (playerTurn === "s" || playerTurn === "scissors") {
           playerTurn = scissors;
-          result();
+          getResult(playerTurn, computerMove);
         } else {
-          console.log('Invalid Input. Try Again...');
-          //console.log("Invalid Input. Try Again...");
+          console.log("Invalid Input. Try Again...");
+          playRound();
         }
       }
     );
-  };
-  playerMove();
+  }
 
-  let result = function () {
-    if (playerTurn === rock && computerMove === rock) {
-      console.log("You chose rock.");
-      console.log("The computer chose rock too!");
-      console.log("It`s a draw!");
-      console.log("See you next time !");
-      return readline.close();
-    } else if (playerTurn === rock && computerMove === paper) {
-      console.log("You chose rock.");
-      console.log("Unfortunately the computer chose paper...");
-      console.log("Sorry! Computer wins!");
-      console.log("See you next time !");
-      return readline.close();
-    } else if (playerTurn === rock && computerMove === scissors) {
-      console.log("You chose rock.");
-      console.log("The computer chose scissors.");
+  function getResult(playerTurn, computerMove) {
+    if (playerTurn === computerMove) {
+      console.log(`You chose ${playerTurn}.`);
+      console.log(`The computer chose ${computerMove} too!`);
+      console.log("It's a draw!");
+    } else if (
+      (playerTurn === "Rock" && computerMove === "Scissors") ||
+      (playerTurn === "Paper" && computerMove === "Rock") ||
+      (playerTurn === "Scissors" && computerMove === "Paper")
+    ) {
+      console.log(`You chose ${playerTurn}.`);
+      console.log(`The computer chose ${computerMove}.`);
       console.log("Congratulations! You win!");
-      console.log("See you next time !");
-      return readline.close();
-    } else if (playerTurn === paper && computerMove === rock) {
-      console.log("You chose paper.");
-      console.log("The computer chose rock.");
-      console.log("Congratulations! You win!");
-      console.log("See you next time !");
-      return readline.close();
-    } else if (playerTurn === paper && computerMove === scissors) {
-      console.log("You chose paper.");
-      console.log("Unfortunately the computer chose scisors...");
-      console.log("Sorry! Computer wins!");
-      console.log("See you next time !");
-      return readline.close();
-    } else if (playerTurn === paper && computerMove === paper) {
-      console.log("You chose paper.");
-      console.log("The computer chose paper too!");
-      console.log("It`s a draw!");
-      console.log("See you next time !");
-      return readline.close();
-    } else if (playerTurn === scissors && computerMove === paper) {
-      console.log("You chose scissors.");
-      console.log("The computer chose paper.");
-      console.log("Congratulations! You win!");
-      console.log("See you next time !");
-      return readline.close();
-    } else if (playerTurn === scissors && computerMove === rock) {
-      console.log("You chose scissors.");
-      console.log("Unfortunately the computer chose rock...");
-      console.log("Sorry! Computer wins!");
-      console.log("See you next time !");
-      return readline.close();
-    } else if (playerTurn === scissors && computerMove === scissors) {
-      console.log("You chose scissors.");
-      console.log("The computer chose scissors too!");
-      console.log("It`s a draw!");
-      console.log("See you next time !");
-      return readline.close();
+    } else {
+      console.log(`You chose ${playerTurn}.`);
+      console.log(`The computer chose ${computerMove}.`);
+      console.log("Unfortunately the computer wins!");
     }
-  };
+
+    console.log("See you next time!");
+    playRound();
+  }
+
+  playRound();
 }
 rockPaperScissors();
